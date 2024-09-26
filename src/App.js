d59e33071dc312bd98f65b2a130c45d86dd91f39
Lernt33 <giorgizautashvili24@gmail.com>
2024-09-26 21:30:58 +0400

    Component sides done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import {Products} from "./consts";
+
+function Product(obj) {
+    return (
+        <div className="product-card">
+            <img src={obj.image} alt={obj.title} className="product-image"/>
+            <div className="product-info">
+                <h2 className="product-title">{obj.title}</h2>
+                <p className="product-description">{obj.description}</p>
+            </div>
+        </div>
+    )
+}
 
 
 function App() {
-  return (<main>
-
-
-  </main>);
+    return (
+        <main>
+            {Products.map((el) => {
+                return <Product key={el.title} {...el} />;
+            })}
+        </main>
+    );
 }
 
 export default App;
