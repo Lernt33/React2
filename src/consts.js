9b58232fc79b1eb2b14272985d99461051c7ef8d
Lernt33 <giorgizautashvili24@gmail.com>
2024-09-26 21:12:08 +0400

    Wiped off everything

---
+++ src/consts.js
@@ -0,0 +1,47 @@
+export const Products = [
+    {
+        title: "Wireless Headphones",
+        description: "High-quality wireless headphones with noise-canceling technology and a long-lasting battery.",
+        image: "https://alta.ge/images/thumbnails/900/650/detailed/298/51jcjQBMLlL._AC_SL1500_.jpg.jpg"
+    },
+    {
+        title: "Smart Watch",
+        description: "A sleek smart watch with fitness tracking, heart rate monitoring, and message notifications.",
+        image: "https://i5.walmartimages.com/asr/607c526a-8af9-45ba-abbd-990844b776f8.e1c0525c5d6006afd7562dba08695c3b.jpeg"
+    },
+    {
+        title: "Bluetooth Speaker",
+        description: "Portable Bluetooth speaker with powerful sound and water-resistant design.",
+        image: "https://www.artis.in/cdn/shop/products/1_f5b3377c-c870-420f-bc6a-5cd4b3a5a7c7.jpg?v=1653639993"
+    },
+    {
+        title: "Gaming Laptop",
+        description: "High-performance gaming laptop with a powerful GPU, fast refresh rate, and RGB keyboard.",
+        image: "https://m.media-amazon.com/images/I/71dwsA3KsfL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+        title: "Action Camera",
+        description: "Compact action camera with 4K video recording and waterproof casing for all adventures.",
+        image: "https://m.media-amazon.com/images/I/813JfSg2qKL.jpg"
+    },
+    {
+        title: "Drone",
+        description: "Advanced drone with HD camera, real-time transmission, and GPS tracking.",
+        image: "https://media.wired.com/photos/65fdfc40f9ff6cdb8f90f943/master/w_1280%2Cc_limit/DJI-Air-3-Offwhite-Background-SOURCE-DJI.jpg/"
+    },
+    {
+        title: "Wireless Mouse",
+        description: "Ergonomically designed wireless mouse with fast response time and smooth tracking.",
+        image: "https://rukminim2.flixcart.com/image/850/1000/xif0q/mouse/3/1/t/dash-2-4ghz-wireless-bluetooth-5-1-mouse-dual-mode-slim-original-imagvwthxhgxtjt6.jpeg?q=90&crop=false"
+    },
+    {
+        title: "Smart Home Speaker",
+        description: "Voice-controlled smart home speaker with advanced AI capabilities and crystal-clear sound.",
+        image: "https://media.wired.com/photos/5cc31f942ba79652e970ca51/master/w_1600%2Cc_limit/Bose-Home-Speaker-500-Angled-SOURCE-Bose.jpg"
+    },
+    {
+        title: "Portable Power Bank",
+        description: "Compact and lightweight power bank with fast charging and multiple USB ports.",
+        image: "https://m.media-amazon.com/images/I/51L3GFbMQHS._AC_UF894,1000_QL80_.jpg"
+    }
+];
